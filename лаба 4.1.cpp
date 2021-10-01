e4ceaa4d9723cae64afc5e48c3d319ec0d325e71
Maria <90962199+MaryaGul@users.noreply.github.com>
2021-10-02 00:47:47 +0300

    Add files via upload

---
+++ лаба 4.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float a, b, s, d;
+	cout << "Enter 1 side of the rectangle" << "\n";
+	cin >> a;
+	cout << "Enter 2 side of the rectangle" << "\n";
+	cin >> b;
+	s = a * b;
+	d = 2 * (a + b);
+	if (a or b <= 0) {
+		cout << "The sides of the rectangle must be positive!" << "\n";
+	}
+	else {
+		cout << "The area of the rectangle S = " << s << "\n";
+		cout << "The perimeter of the rectangle P = " << d << "\n";
+	}
+	return 0;
+}
+
