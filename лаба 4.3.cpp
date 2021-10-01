46741ef4aea88165d6c9313788eb1e488aca7b61
Maria <90962199+MaryaGul@users.noreply.github.com>
2021-10-02 01:35:45 +0300

    Add files via upload

---
+++ лаба 4.3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float a, b, Sr;
+	cout << "Enter 1 number" << "\n";
+	cin >> a;
+	cout << "Enter 2 number" << "\n";
+	cin >> b;
+	Sr = (a + b) / 2;
+	cout << "The average Sr= " << Sr << "\n";
+	return 0;
+}
+
