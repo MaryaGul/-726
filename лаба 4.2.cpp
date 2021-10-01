0119de7b611c9f622bc684cb41db795471ae8d91
Maria <90962199+MaryaGul@users.noreply.github.com>
2021-10-02 01:06:14 +0300

    Add files via upload

---
+++ лаба 4.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float L, d, P = 3.14;
+	cout << "Enter the daimetr of the circle" << "\n";
+	cin >> d;
+	L = P * d;
+	if (d <= 0) {
+		cout << "The daimetr of the circle must be positive!" << "\n";
+	}
+	else {
+		cout << "The circumference L = " << L << "\n";
+	}
+	return 0;
+}
