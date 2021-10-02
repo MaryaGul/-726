68414f4d571f1358dd6ff9ae7dc3b0eade5cf461
Maria <90962199+MaryaGul@users.noreply.github.com>
2021-10-02 13:43:06 +0300

    Update лаба 4.5.cpp

--- лаба 4.5.cpp
+++ лаба 4.5.cpp
@@ -8,15 +8,16 @@ int main() {
 	cin >> a;
 	cout << "Please enter a nonzero 2 number" << "\n";
 	cin >> b;
-	s = fabs(a)  + fabs(b) ;
-	r = fabs(a) - fabs(b);
-	p = fabs(a) * fabs(b);
-	ch = fabs(a) / fabs(b);
 	if (a and b != 0) {
+		s = fabs(a)  + fabs(b) ;
+		r = fabs(a) - fabs(b);
+		p = fabs(a) * fabs(b);
+		ch = fabs(a) / fabs(b);
 		cout << "Sum of modules of numbers s = " << s << "\n";
 		cout << "Difference of modules of numbersr = " << r << "\n";
 		cout << "Product of modules of numbersp = " << p << "\n";
 		cout << "Quotient of modules of numbersch = " << ch << "\n";
+		
 	}
 	else {
 		cout << "The number must be nonzero!" << "\n";
