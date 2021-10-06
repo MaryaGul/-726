6a74df6b06d3893589c11c3dc1c8cbfa22826780
Maria <90962199+MaryaGul@users.noreply.github.com>
2021-10-07 01:48:04 +0300

    Add files via upload

---
+++ лаба 5.4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    float x1, x3, y1, y3, p, s;
+    cout << "Enter the coordinates of the first vertex of the rectangle(x1, y1)" << endl;
+    cin >> x1 >> y1;
+    cout << "Enter the coordinates of the second vertex that is opposite to the first vertex of the rectangle(x3,y3)" << endl;
+    cin >> x3 >> y3;
+    s = abs((y3 - y1) * (x3 - x1));
+    p = abs(2 * ((y3 - y1) + (x3 - x1)));
+    cout << "The area of the rectangle S = " << s << "\n";
+    cout << "The perimeter of the rectangle P = " << p << "\n";
+    return 0;
+}
+
