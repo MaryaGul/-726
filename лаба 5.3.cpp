719044399985952bcbdc510af749b4bbc227c013
Maria <90962199+MaryaGul@users.noreply.github.com>
2021-10-07 00:49:37 +0300

    Add files via upload

---
+++ лаба 5.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, c, ac, bc, p;
+    cout << "Enter the coordinate of point A(one number)" << endl;
+    cin >> a;
+    cout << "Enter the coordinate of point B(one number)" << endl;
+    cin >> b;
+    cout << "Enter the coordinate of point C, which is located between points A and B(A<C<B)" << endl;
+    cin >> c;
+    if (a < c and c < b) {
+        ac = c - a;
+        bc = b - c;
+        p = ac * bc;
+        cout << "Product of line segments AC*BC=" << p << endl;
+    }
+    else {
+        cout << "Coordinate of point c must be greater than coordinate A and less than coordinate B! " << endl;
+    }
+    return 0;
+}
